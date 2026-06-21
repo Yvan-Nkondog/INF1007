3d20a11248f851a53a27b604630c0fb6f24f3248
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-06-21 18:04:29 -0400

    Exercice 4, complet, tous les tests fonctionnent.

--- Tutorials/TP1/exercice4.py
+++ Tutorials/TP1/exercice4.py
@@ -4,15 +4,18 @@ import math
 def calculerPosition(positionInitiale, vitesseInitiale, duree, vitesseFinale):
     # TODO faites les calculs intermediaires, vous pouvez initialiser des variables locales.
     a = 0
+    # Harmonisation des unités (en mètre, seconde, m/s, m/s^2)
+    CONSTANTE_CONVERSION_EN_METRE_PAR_SECONDE = 1000 / 3600
+    vitesse_initiale_metre_par_seconde = vitesseInitiale * CONSTANTE_CONVERSION_EN_METRE_PAR_SECONDE
+    vitesse_finale_metre_par_seconde = vitesseFinale * CONSTANTE_CONVERSION_EN_METRE_PAR_SECONDE
+
+    # Calculer l'accération à partir de la formule fournie (en m / s^2)
+    acceleration = (vitesse_finale_metre_par_seconde - vitesse_initiale_metre_par_seconde) / duree
+
+    # Calculer la variation de position, à partir de la formule fournie.
+    delta_position = (vitesse_initiale_metre_par_seconde * duree) + (0.5 * acceleration * (duree ** 2))
     
     # TODO calculer la position finale, assigner la valeur à la variable "positionFinale"
-    positionFinale = 0
+    positionFinale = positionInitiale + delta_position
 
     return positionFinale
-
-if __name__ == '__main__':
-    positionInitiale = int(input("Entrez la position initiale en mètre: "))
-    vitesseInitiale = int(input("Entrez la vitesse initiale en km/h: "))
-    duree = int(input("Entrez la duree en secondes: "))
-    vitesseFinale = int(input("Entrez la vitesseFinale en km/h: "))
-    print(calculerPosition(positionInitiale, vitesseInitiale, duree, vitesseFinale))
