01b88ca595371039c380775b52f5008a1bb0a55f
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-06-21 18:20:53 -0400

    Exercice 5, tous les tests fonctionnent.

--- Tutorials/TP1/exercice5.py
+++ Tutorials/TP1/exercice5.py
@@ -1,15 +1,11 @@
 def pointDeRencontre(v1, v2, distance):
     # TODO faites les calculs intermediaires, vous pouvez initialiser des variables locales.
-    
+    # Calculer la proportion de déplacement relativement au véhicule 1
+    # (NB : La somme des deux proportions devrait donner une unité).
+    fraction_de_position_relative_a_v1 = (v1) / (v1 + v2)
     
     # TODO calculer la position de rencontre, assignez la valeur à la variable "positionRencontre"
     
-    positionRencontre = 0
+    positionRencontre = distance * fraction_de_position_relative_a_v1
 
     return positionRencontre
-
-if __name__ == '__main__':
-    v1 = int(input("Entrez v1: "))
-    v2 = int(input("Entrez v2: "))
-    distance = int(input("Entrez la distance: "))
-    print(pointDeRencontre(v1, v2, distance))
