dddf2948e316a3ac26515800b4065d05d5bb4e25
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-06-24 07:57:21 -0400

    Fichier molecule.py : tous les tests fonctionnent.

--- Project/Projet2/Molecule.py
+++ Project/Projet2/Molecule.py
@@ -6,32 +6,48 @@
 # Modifier le   : 
 
 from random import randint, randrange
+import math
+
 
 def creerMolecule(x, y, dx, dy, rayon):
     #TODO 3.1.1 Créer le dictionnaire pour représenter une molécule
-    print('hello')
-    return ...
+    return {"x": x, "y": y, "dx": dx, "dy": dy, "rayon": rayon}
+
+
+# Fonction supplémentaire qui permet de vérifier si un paramètre est de type
+# molécule.
+def estMolecule(argument):
+    return (
+        isinstance(argument, dict)
+        and set(argument.keys()) == {"x", "y", "dx", "dy", "rayon"}
+    )
 
 
 def moleculesSeTouche(mol_1, mol_2):
     #TODO 3.1.2 Implémenter la formule pour vérifier si deux molécules se touche
     # Renvoi vrai si les molécules se touchent, faux si non
+    if not (estMolecule(mol_1) and estMolecule(mol_2)):
+        raise TypeError("Le paramètre doit être de type 'molécule'.")
     
-    return ...
+    return math.sqrt(((mol_2["x"] - mol_1["x"])**2) + 
+                         ((mol_2["y"] - mol_1["y"])**2)) < ((mol_1["rayon"]) + (mol_2["rayon"]))
 
 
 def deplacerMolecule(mol):
     #TODO 3.1.3 Faire le déplacement de la molécule
-    
-    return ...
+    if not estMolecule(mol):
+        raise TypeError("Le paramètre doit être de type 'molécule'.")
+    mol["x"] += mol["dx"]
+    mol["y"] += mol["dy"]
+    return mol
 
     
 #####################################################
 # Donner
 #####################################################
-def ajusteDirApresCollision(mol_1, mol_2)
+def ajusteDirApresCollision(mol_1, mol_2):
     deltaX = mol_2['x'] - mol_1['x']
-    dVx = 0;
+    dVx = 0
 
     if (deltaX == 0.0):
         dVy = mol_2['y'] - mol_1['y']
@@ -47,43 +63,68 @@ def ajusteDirApresCollision(mol_1, mol_2)
 
     return mol_1, mol_2
 
+
 def creerListMolecules(hauteur,xmin,xmax,nbMolecules):
     #TODO 3.1.5 Remplir la liste de molécule comme déctrit dans le README
     # vous pouvez utiliser rayon = randrange(10,30,2) et randint pour x,y,dx,dy
 
-    return ...
+    list_molecules = []
+    for i in range(nbMolecules):
+        rayon = randrange(10, 30, 2)
+        # La position de la molécule ne peut pas dépasser la paroi sur
+        # l'axe x. Il faut par conséquent assurer que le centre de la molécule
+        # ne déborde pas une distance (xmin + rayon) ou (xmax - rayon).
+        # NB: La position d'une molécule est mésurée à partir de son centre.
+        x = randint(xmin + rayon, xmax - rayon)
+        y = randint(rayon, hauteur - rayon)
+        dx = randint(round(xmin/2), round(xmax/2))
+        dy = randint(0, round(hauteur/2))
+
+        # Créer la molécule à partir des données aléatoires et ajouter là
+        # à la liste des molécules.
+        list_molecules.append(creerMolecule(x, y, dx, dy, rayon))
+    return list_molecules
+
 
 def inverseDirMolecule(mol, paroiG, paroiD, hauteur):
-    #TODO 3.1.6 Implémenter la fonction décrite dans le README
-    # inverseDirMolecule inverse la direction de la molécule
+    #TODO 3.1.6 Implémenter la fonction décrite dans le README.
+    # InverseDirMolecule inverse la direction de la molécule.
     # Cette fonction reçoit en entrer quatre paramètres:
-    # la molécule les parois gauche et droit du chaque côté du réservoir et la hauteurdu reservoir.
+    # la molécule les parois gauche et droit du chaque côté du réservoir et la hauteur du reservoir.
     # Si la molécule touche une des parois du réservoir un faut la reposition à la limite
     # du réservoir et inverser sa direction en vitesse.
 
+    # Valider que le paramètre "mol" est de type "molecule".
+    if not estMolecule(mol):
+        raise TypeError("Le paramètre doit être de type 'molécule'.")
+
     # Si la molécule touche la paroi gauche du réservoir en x
-
+    if ((mol["x"] - mol["rayon"]) <= paroiG):
         # Repositionner la molécule et changer sa direction dx
-
+        mol["x"] = paroiG + mol["rayon"]
+        mol["dx"] *= -1
 
     # Si la molécule touche la paroi droite du réservoir en x
-
+    if ((mol["x"] + mol["rayon"]) >= paroiD):
         # Repositionner la molécule et changer sa direction dx
+        mol["x"] = paroiD - mol["rayon"]
+        mol["dx"] *= -1
 
-
-
-    # Si la molécule touche la paroi gauche du réservoir en x
-
+    # Si la molécule touche la paroi gauche du réservoir en y
+    if ((mol["y"] - mol["rayon"]) <= 0):
         # Repositionner la molécule et changer sa direction dy
-
+        mol["y"] = mol["rayon"]
+        mol["dy"] *= -1
 
     # Si la molécule touche la paroi droite du réservoir en y
-
+    if ((mol["y"] - mol["rayon"]) >= hauteur):
         # Repositionner la molécule et changer sa direction dy
+        mol["y"] = hauteur - mol["rayon"]
+        mol["dy"] *= -1
 
+    # Retourner la molécule avec la direction inversée.
+    return mol
 
-
-    return ...
 
 if __name__ == '__main__':
     # Test creerMolecule
