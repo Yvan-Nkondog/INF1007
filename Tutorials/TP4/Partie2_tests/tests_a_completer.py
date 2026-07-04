280624da2eabbbf87c9404a5c407f0a978eff2ab
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-04 14:20:02 -0400

    Fichier de test à compléter

--- Tutorials/TP4/Partie2_tests/tests_a_completer.py
+++ Tutorials/TP4/Partie2_tests/tests_a_completer.py
@@ -6,25 +6,23 @@ import fonctions_a_tester as fonctions
 class TestFizzBuzz(TestCase):
     def test_fizz_buzz_3(self):
         # TODO Tester avec un multiple de 3
-
-        self.assertEqual(fonctions.fizz_buzz(12), 'fizz')
+        pass
 
     def test_fizz_buzz_5(self):
         # TODO Tester avec un multiple de 5
 
-        self.assertEqual(fonctions.fizz_buzz(10), 'buzz')
+        pass
 
     def test_fizz_buzz_3_5(self):
         # TODO Tester avec un multiple de 3 et 5
 
-        self.assertEqual(fonctions.fizz_buzz(15), 'fizzbuzz')
+        pass                                                                                                                                                                                                                                                                                                                                         
 
     def test_fizz_buzz_non_facteur(self):
         # TODO Tester avec un nombre qui nombre'a pas 3 et 5 comme facteur
         #  et assurez-vous que la valeur en sotie soit une string
 
-        self.assertEqual(fonctions.fizz_buzz(30), 'fizzbuzz')
-        self.assertEqual(type(fonctions.fizz_buzz(30)), str)
+        pass
 
 
 class TestResoudreEquation(TestCase):
@@ -32,17 +30,17 @@ class TestResoudreEquation(TestCase):
         # TODO Tester avec un polynome sans racines réelles
         #  et assurez-vous que la valeur en sortie est None
 
-        self.assertEqual(fonctions.resoudre_equation(1, 0, 1), None)
+       pass
 
     def test_resoudre_equation_une_racine(self):
         # TODO Tester avec un polynome avec une seule solution
 
-        self.assertEqual(fonctions.resoudre_equation(1, -4, 4), 2)
+        pass
 
     def test_resoudre_equation_deux_racine(self):
         # TODO Tester avec un polynome avec deux solutions
 
-        self.assertEqual(fonctions.resoudre_equation(1, 0, -16), (4, -4))
+        pass
 
 
 if __name__ == '__main__':
